9a38fa2245c144a6aa1df7f9e6177113ee2d08a3
mdodovic <32821985+mdodovic@users.noreply.github.com>
2021-01-29 15:56:55 +0100

    public test added

---
+++ Public-test/test.h
@@ -0,0 +1,12 @@
+#pragma once
+#define MASK (0xA5)
+#include "slab.h"
+
+struct data_s {
+	int id;
+	kmem_cache_t *shared;
+	int iterations;
+};
+
+
+void run_threads(void(*work)(void*), struct data_s* data, int num);
